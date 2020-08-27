20b1018b901e87f08985998ab9b69eaf42e5a5c1
rabbymohiuddin <rabbymohiuddin@gmail.com>
2020-08-27 23:56:08 +0600

    all removed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Nav from "./components/Nav/Nav";
-import Home from "./components/Home/Home";
-import Pricing from "./components/Pricing/Pricing";
-import Purchase from "./components/Purchase/Purchase";
-import StripePayment from "./components/StripePayment/StripePayment";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <>
-      <Router>
-        <Nav></Nav>
-        <div className="py-5">
-          <Switch>
-            <Route exact path="/" component={Home}></Route>
-            <Route path="/pricing" component={Pricing}></Route>
-            <Route path="/purchase" component={Purchase}></Route>
-            <Route path="/payment" component={StripePayment}></Route>
-          </Switch>
-        </div>
-      </Router>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
